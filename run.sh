e53e2851bb52c1eaa5e10c571844bbda690e99a3
cwyz-dev <chris@wwwoods.xyz>
2026-05-26 11:43:22 -0600

    Starting

--- run.sh
+++ run.sh
@@ -4,5 +4,6 @@ podman build -f Containerfile -t "$RUNNER_IMAGE" -q
 
 podman run --rm \
   -v ansible:/ansible -v resources:/resources \
+  -v "$HOME/.ssh/id_ed25519:/root/.ssh/id_ed25519:ro" \
   "$RUNNER_IMAGE" \
-  sh -c 'ansibel-playbook -i inventories/hosts.yaml playbooks/deploy.yaml'
+  sh -c 'ansible-playbook -i inventories/hosts.yaml playbooks/deploy.yaml'
